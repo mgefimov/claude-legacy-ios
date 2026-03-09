a1761fa7d0f443f5369fdff99f3e1390e5faeadb
Maksim Efimov <mg.efimov97@gmail.com>
2026-03-09 21:36:24 +0900

    fix nonce for ios 15.0 and lower

--- ClaudeLegacy Extension/Resources/content.js
+++ ClaudeLegacy Extension/Resources/content.js
@@ -1,36 +1,50 @@
- (function () {
-   function patchCode(code) {
-     // 1.  Lookbehind assertion: (?<=...), (?<!...)
-     code = code.replace(/\(\?<[=!]([^()]*(?:\([^()]*\))*[^()]*)\)/g, "");
+(function () {
+  function patchCode(code) {
+    // 1.  Lookbehind assertion: (?<=...), (?<!...)
+    code = code.replace(/\(\?<[=!]([^()]*(?:\([^()]*\))*[^()]*)\)/g, "");
 
-     // 2. Class static blocks: static{__name(this,"...")}
-     code = code.replace(/static\s*\{\s*__name\(this\s*,\s*"[^"]*"\)\s*\}/g, "");
+    // 2. Class static blocks: static{__name(this,"...")}
+    code = code.replace(/static\s*\{\s*__name\(this\s*,\s*"[^"]*"\)\s*\}/g, "");
 
-     return code;
-   }
-   const observer = new MutationObserver((mutations) => {
-     for (const mutation of mutations) {
-       for (const node of mutation.addedNodes) {
-         if (node.tagName === "SCRIPT" && node.src) {
-           node.type = "javascript/blocked";
-           const parent = node.parentNode;
-           const src = node.src;
+    return code;
+  }
 
-           fetch(src)
-             .then((r) => r.text())
-             .then((code) => {
-               const patched = patchCode(code);
-               const newScript = document.createElement("script");
-               newScript.textContent = patched;
-               (parent || document.head).appendChild(newScript);
-             });
-         }
-       }
-     }
-   });
+  function getNonce() {
+    var nonce = '';
+    var s = document.querySelector('script[nonce]');
+    if (s) nonce = s.nonce || s.getAttribute('nonce');
+    return nonce;
+  }
 
-   observer.observe(document.documentElement, {
-     childList: true,
-     subtree: true,
-   });
- })();
+  const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      for (const node of mutation.addedNodes) {
+        if (node.tagName === "SCRIPT" && node.src) {
+          node.type = "javascript/blocked";
+          const parent = node.parentNode;
+          const src = node.src;
+
+          fetch(src)
+            .then((r) => r.text())
+            .then((code) => {
+              const patched = patchCode(code);
+              const newScript = document.createElement("script");
+
+              newScript.setAttribute('data-orig', src)
+
+              const nonce = getNonce()
+              newScript.setAttribute('nonce', nonce);
+
+              newScript.textContent = patched;
+              (parent || document.head).appendChild(newScript);
+            });
+        }
+      }
+    }
+  });
+
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  });
+})();
