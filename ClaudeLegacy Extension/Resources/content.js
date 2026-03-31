f6d5e9613633d494a886da246154da3765a60381
Maksim Efimov <maksim.efimov@sbermarket.ru>
2026-03-31 22:13:39 +0900

    fix after build system change

--- ClaudeLegacy Extension/Resources/content.js
+++ ClaudeLegacy Extension/Resources/content.js
@@ -1,43 +1,22 @@
 (function () {
-  function patchCode(code) {
-    // 1.  Lookbehind assertion: (?<=...), (?<!...)
-    code = code.replace(/\(\?<[=!]([^()]*(?:\([^()]*\))*[^()]*)\)/g, "");
+  const BASE_URL =
+    "https://assets-proxy.anthropic.com/claude-ai/v2/assets/v1";
+  // const BASE_URL = "http://192.168.1.136:3000";
 
-    // 2. Class static blocks: static{__name(this,"...")}
-    code = code.replace(/static\s*\{\s*__name\(this\s*,\s*"[^"]*"\)\s*\}/g, "");
-
-    return code;
-  }
-
-  function getNonce() {
-    var nonce = '';
-    var s = document.querySelector('script[nonce]');
-    if (s) nonce = s.nonce || s.getAttribute('nonce');
-    return nonce;
-  }
+  window.LegacyTranspiler.init({
+    BASE_URL,
+    runScript: (code) => {
+      window.webkit.messageHandlers.patchScript.postMessage(code);
+    },
+  });
 
   const observer = new MutationObserver((mutations) => {
     for (const mutation of mutations) {
       for (const node of mutation.addedNodes) {
         if (node.tagName === "SCRIPT" && node.src) {
           node.type = "javascript/blocked";
-          const parent = node.parentNode;
           const src = node.src;
-
-          fetch(src)
-            .then((r) => r.text())
-            .then((code) => {
-              const patched = patchCode(code);
-              const newScript = document.createElement("script");
-
-              newScript.setAttribute('data-orig', src)
-
-              const nonce = getNonce()
-              newScript.setAttribute('nonce', nonce);
-
-              newScript.textContent = patched;
-              (parent || document.head).appendChild(newScript);
-            });
+          window.LegacyTranspiler.loadCode(src)
         }
       }
     }
--- ClaudeLegacy/ViewController.m
+++ ClaudeLegacy/ViewController.m
@@ -21,7 +21,17 @@
 - (void) injectPatch {
     NSBundle *extensionBundle = [NSBundle bundleWithURL:[NSBundle.mainBundle URLForResource:@"ClaudeLegacy Extension" withExtension:@"appex" subdirectory:@"PlugIns"]];
     NSURL *scriptURL = [extensionBundle URLForResource:@"content" withExtension:@"js"];
+    
+    NSString *js = [NSString stringWithContentsOfURL:scriptURL encoding:NSUTF8StringEncoding error:nil];
+    if (js) {
+        WKUserScript *userScript = [[WKUserScript alloc] initWithSource:js injectionTime:WKUserScriptInjectionTimeAtDocumentStart forMainFrameOnly:YES];
+        [_webView.configuration.userContentController addUserScript:userScript];
+    }
+}
 
+- (void) injectTranspiler {
+    NSURL *scriptURL = [NSBundle.mainBundle URLForResource:@"legacy-transpiler" withExtension:@"js"];
+    
     NSString *js = [NSString stringWithContentsOfURL:scriptURL encoding:NSUTF8StringEncoding error:nil];
     if (js) {
         WKUserScript *userScript = [[WKUserScript alloc] initWithSource:js injectionTime:WKUserScriptInjectionTimeAtDocumentStart forMainFrameOnly:YES];
@@ -31,16 +41,16 @@
 
 - (void)injectCustomCSS {
     NSString *css = @"button[data-testid='login-with-google'] { display: none !important; }"
-                     "button[data-testid='login-with-google'] + p { display: none !important; }";
+    "button[data-testid='login-with-google'] + p { display: none !important; }";
     NSString *js = [NSString stringWithFormat:
-        @"(function(){"
-         "var s=document.createElement('style');"
-         "s.textContent=%@;"
-         "document.head.appendChild(s);"
-         "})()", [self jsStringLiteral:css]];
+                    @"(function(){"
+                    "var s=document.createElement('style');"
+                    "s.textContent=%@;"
+                    "document.head.appendChild(s);"
+                    "})()", [self jsStringLiteral:css]];
     WKUserScript *script = [[WKUserScript alloc] initWithSource:js
                                                   injectionTime:WKUserScriptInjectionTimeAtDocumentEnd
-                                                 forMainFrameOnly:YES];
+                                               forMainFrameOnly:YES];
     [_webView.configuration.userContentController addUserScript:script];
 }
 
@@ -53,29 +63,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     self.view.backgroundColor = [UIColor colorWithDynamicProvider:^UIColor *(UITraitCollection *tc) {
         return tc.userInterfaceStyle == UIUserInterfaceStyleDark
-            ? [UIColor colorWithRed:0x26/255.0 green:0x26/255.0 blue:0x24/255.0 alpha:1.0]   // #262624
-            : [UIColor colorWithRed:0xF8/255.0 green:0xF7/255.0 blue:0xF3/255.0 alpha:1.0];  // #F8F7F3
+        ? [UIColor colorWithRed:0x26/255.0 green:0x26/255.0 blue:0x24/255.0 alpha:1.0]   // #262624
+        : [UIColor colorWithRed:0xF8/255.0 green:0xF7/255.0 blue:0xF3/255.0 alpha:1.0];  // #F8F7F3
     }];
-
+    
     UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
     [refreshControl addTarget:self action:@selector(handleRefresh:) forControlEvents:UIControlEventValueChanged];
     _webView.scrollView.refreshControl = refreshControl;
-
+    
     _webView.opaque = NO;
     _webView.backgroundColor = UIColor.clearColor;
     _webView.navigationDelegate = self;
     _webView.scrollView.scrollEnabled = YES;
-
-    [_webView.configuration.userContentController addScriptMessageHandler:self name:@"controller"];
+    
+    [self.webView.configuration.userContentController addScriptMessageHandler:self name:@"patchScript"];
     
     [self injectCustomCSS];
+    [self injectTranspiler];
     [self injectPatch];
     [PolyfillsLoader injectPolyfillsIntoController:_webView.configuration.userContentController];
-
+    
     [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://claude.ai"]]];
+//    [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://192.168.1.136:3000"]]];
 }
 
 - (UIStatusBarStyle)preferredStatusBarStyle {
@@ -90,9 +102,25 @@
     [webView.scrollView.refreshControl endRefreshing];
 }
 
-- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message
+// Replace your handler method with:
+- (void)userContentController:(WKUserContentController *)userContentController
+      didReceiveScriptMessage:(WKScriptMessage *)message
 {
-    // Override point for customization.
+    if (![message.name isEqualToString:@"patchScript"]) {
+        return;
+    }
+    
+    NSString *code = message.body;
+    
+    NSString *wrapped = [NSString stringWithFormat:@"%@\n;'ok'", code];
+
+    [self.webView evaluateJavaScript:wrapped completionHandler:^(id res, NSError *err) {
+        if (err) {
+            NSLog(@"[evaluateJavaScript]: fail %@", code);
+        } else {
+            NSLog(@"[evaluateJavaScript]: success"); // always return a serializable value
+        }
+    }];
 }
 
 @end
